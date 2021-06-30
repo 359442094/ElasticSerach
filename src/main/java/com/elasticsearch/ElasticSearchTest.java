2a9ca08ba356b96b58ea0de1c64eb8c64c051980
chenjie <359442094@qq.com>
2021-06-30 18:08:07 +0800

    add pageList

--- src/main/java/com/elasticsearch/ElasticSearchTest.java
+++ src/main/java/com/elasticsearch/ElasticSearchTest.java
@@ -26,7 +26,7 @@ import java.util.List;
  */
 public class ElasticSearchTest {
 
-    private void search() {
+    private void queryData() {
         SearchRequest searchRequest = new SearchRequest();
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
         searchSourceBuilder.query(QueryBuilders.matchAllQuery());
@@ -35,8 +35,9 @@ public class ElasticSearchTest {
 
     public static void main(String[] args) throws IOException {
         String indexName = "test111";
+        String id = "1234";
         // 创建连接
-        //ElasticSearchUtil.createConnection();
+        ElasticSearchUtil.createConnection();
 
         //查询单个索引
         //ElasticSearchUtil.getIndex(indexName);
@@ -45,14 +46,34 @@ public class ElasticSearchTest {
         //删除索引
         //ElasticSearchUtil.deleteIndex(indexName);
 
-        //创建索引关联的内容
-        testSaveOrUpdateData(indexName);
+        //创建或者修改索引内容
+        //testSaveOrUpdateData(indexName,id);
 
+        //根据编号进行查询
+        //ElasticSearchUtil.queryDataById(indexName,id);
+
+        //删除索引内容
+        ElasticSearchUtil.deleteDataById(indexName, id);
+
+        //普通查询全部索引|指定索引中内容
+        //List<User> list = ElasticSearchUtil.queryDataByIndexName(indexName, User.class);
+        //分页查询全部索引|指定索引中内容
+        int pageIndex = 0;
+        int pageSize = 5;
+        List<User> pageList = ElasticSearchUtil.queryPageDataByIndexName(indexName, User.class, pageIndex, pageSize);
+
+
+        ElasticSearchUtil.closeConnection();
     }
 
-    private static void testSaveOrUpdateData(String indexName) {
+    /**
+     * 创建或者修改索引内容
+     *
+     * @param indexName
+     * @param id
+     */
+    private static void testSaveOrUpdateData(String indexName, String id) {
         List<User> users = User.users;
-        String id = "1234";
         String jsonString = JSONObject.toJSONString(users.get(0));
         ElasticSearchUtil.saveOrUpdateIndexAndData(indexName, id, jsonString);
     }
--- src/main/java/com/elasticsearch/util/ElasticSearchUtil.java
+++ src/main/java/com/elasticsearch/util/ElasticSearchUtil.java
@@ -5,18 +5,35 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
 import org.elasticsearch.ElasticsearchStatusException;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.*;
+import org.elasticsearch.client.core.GetSourceRequest;
+import org.elasticsearch.client.core.GetSourceResponse;
 import org.elasticsearch.client.indices.*;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -139,42 +156,6 @@ public class ElasticSearchUtil {
     }
 
     /**
-     * 创建或者修改索引内容
-     * @param indexName
-     * @param id
-     * @param sourceJson
-     * @return
-     */
-    public static boolean saveOrUpdateIndexAndData(String indexName, String id, String sourceJson) {
-        if (client == null) {
-            init();
-        }
-        if (StringUtils.isEmpty(id) || StringUtils.isEmpty(sourceJson)) {
-            log.error("索引内容{}创建失败:参数id或sourceJson缺失", indexName);
-            return false;
-        }
-        IndexResponse response = null;
-        try {
-            IndexRequest request = new IndexRequest(indexName);
-            request.id(id);
-            request.source(sourceJson, XContentType.JSON);
-            response = client.index(request, RequestOptions.DEFAULT);
-            boolean resultFlag = response != null && response.getResult() != null
-                    && "CREATED".equals(response.getResult().toString())
-                    || "UPDATED".equals(response.getResult().toString());
-            if (resultFlag) {
-                log.info("索引内容{}创建|修改成功", indexName);
-            } else {
-                log.error("索引内容{}创建|修改失败", indexName);
-            }
-            return resultFlag;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    /**
      * 删除索引
      *
      * @param indexName
@@ -208,5 +189,206 @@ public class ElasticSearchUtil {
         return false;
     }
 
+    /**
+     * 创建或者修改索引内容
+     *
+     * @param indexName
+     * @param id
+     * @param sourceJson
+     * @return
+     */
+    public static boolean saveOrUpdateIndexAndData(String indexName, String id, String sourceJson) {
+        if (client == null) {
+            init();
+        }
+        if (StringUtils.isEmpty(indexName) || StringUtils.isEmpty(id) || StringUtils.isEmpty(sourceJson)) {
+            log.error("index:{}内容创建失败:参数indexName,id或sourceJson缺失", indexName);
+            return false;
+        }
+        IndexResponse response = null;
+        try {
+            IndexRequest request = new IndexRequest(indexName);
+            request.id(id);
+            request.source(sourceJson, XContentType.JSON);
+            response = client.index(request, RequestOptions.DEFAULT);
+            boolean resultFlag = response != null && response.getResult() != null
+                    && "CREATED".equals(response.getResult().toString())
+                    || "UPDATED".equals(response.getResult().toString());
+            if (resultFlag) {
+                log.info("index:{}内容创建|修改成功,id:{}", indexName, id);
+            } else {
+                log.error("index:{}内容创建|修改失败,id:{}", indexName, id);
+            }
+            return resultFlag;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * id可以不填写，但是会删除索引全部内容
+     *
+     * @param indexName
+     * @param id
+     * @return
+     */
+    public static boolean deleteDataById(String indexName, String id) {
+        if (client == null) {
+            init();
+        }
+        if (StringUtils.isEmpty(indexName) || StringUtils.isEmpty(id)) {
+            log.error("index:{}内容创建失败:参数id或indexName缺失", indexName);
+            return false;
+        }
+        try {
+            DeleteResponse response = client.delete(new DeleteRequest(indexName, id), RequestOptions.DEFAULT);
+            return response != null && response.status() == RestStatus.OK;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 根据编号进行查询
+     *
+     * @param indexName
+     * @param id
+     * @return
+     */
+    public static GetResponse queryDataById(String indexName, String id) {
+        if (client == null) {
+            init();
+        }
+        if (StringUtils.isEmpty(indexName) || StringUtils.isEmpty(id)) {
+            log.error("queryDataById,参数id或indexName缺失", indexName);
+            return null;
+        }
+        GetResponse response = null;
+        try {
+            GetRequest request = new GetRequest(indexName);
+            request.id(id);
+            response = client.get(request, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+
+    /**
+     * 普通查询
+     *
+     * @param
+     * @return
+     */
+    public static SearchResponse queryData(SearchRequest request) {
+        if (client == null) {
+            init();
+        }
+        try {
+            return client.search(request, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 分页查询
+     *
+     * @param request
+     * @return
+     */
+    public static SearchResponse queryPageData(SearchRequest request, Integer pageIndex, Integer pageSize) {
+        if (client == null) {
+            init();
+        }
+        try {
+            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+            searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+            // 从第几条开始
+            int from;
+            if (pageIndex <= 1) {
+                from = 0;
+            } else {
+                from = pageIndex;
+            }
+            from = (from * pageSize);
+            searchSourceBuilder.from(from);
+            //查询多少条
+            searchSourceBuilder.size(pageSize);
+            //排序
+            //searchSourceBuilder.sort();
+            request.source(searchSourceBuilder);
+            return client.search(request, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 普通查询全部索引|指定索引中内容
+     * 索引名称可以为空
+     *
+     * @param indexName 索引名称
+     * @param myClass   返回的类型
+     * @return List
+     */
+    public static List queryDataByIndexName(String indexName, Class myClass) {
+        if (client == null) {
+            init();
+        }
+        List results = new ArrayList();
+        SearchRequest request = new SearchRequest(indexName);
+        SearchResponse response = queryData(request);
+        if (response == null) {
+            return null;
+        }
+        //总记录数
+        int totalCount = response.getHits().getHits().length;
+        if (totalCount > 0) {
+            for (SearchHit hit : response.getHits().getHits()) {
+                String dataJson = hit.getSourceAsString();
+                results.add(JSONObject.parseObject(dataJson, myClass));
+            }
+        }
+        System.out.println("results:" + results);
+        return results;
+    }
+
+    /**
+     * 分页查询全部索引|指定索引中内容
+     * @param indexName
+     * @param myClass
+     * @param pageIndex
+     * @param pageSize
+     * @return
+     */
+    public static List queryPageDataByIndexName(String indexName, Class myClass,Integer pageIndex, Integer pageSize) {
+        if (client == null) {
+            init();
+        }
+        List results = new ArrayList();
+        SearchRequest request = new SearchRequest(indexName);
+        SearchResponse response = queryPageData(request,pageIndex,pageSize);
+        if (response == null) {
+            return null;
+        }
+        //总记录数
+        int totalCount = response.getHits().getHits().length;
+        //log.info("总记录数:"+response.getHits().getTotalHits().value);
+        log.info("总记录数:"+totalCount);
+        if (totalCount > 0) {
+            for (SearchHit hit : response.getHits().getHits()) {
+                String dataJson = hit.getSourceAsString();
+                results.add(JSONObject.parseObject(dataJson, myClass));
+            }
+        }
+        System.out.println("results:" + results);
+        return results;
+    }
 
 }
